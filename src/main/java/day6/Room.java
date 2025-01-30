41a5104103f2ca00bd372c4bf919aabbc31f7380
Georg Plafinger <georg.plafinger@datev.de>
2025-01-30 16:22:21 +0100

    initialze room size from input

--- src/main/java/day6/Room.java
+++ src/main/java/day6/Room.java
@@ -1,17 +1,48 @@
 package day6;
 
+import java.io.Reader;
+import java.util.function.IntConsumer;
+
 class Room {
+
+    private int length = 0;
+    private int width = 0;
+    int currentLineWidth = 0;
 
     record Size(int width, int length) { }
 
-    private final Size size;
+    private Size size;
 
-    public Room(String roomMap) {
-        size = new Size(0, 0);
+    public Room(Reader input) {
+        initializeFrom(input);
+    }
+
+    private void initializeFrom(Reader input) {
+        Map.codePoints(input).forEach(new IntConsumer() {
+            @Override
+            public void accept(int codePoint) {
+                switch ((char) codePoint) {
+                    case '\n' -> {
+                        increaseLength();
+                        increaseWidthOnDemand();
+                    }
+                    default -> ++currentLineWidth;
+                }
+            }
+        });
+    }
+
+    private void increaseLength() {
+        ++length;
+    }
+
+    private void increaseWidthOnDemand() {
+        width = Math.max(width, currentLineWidth);
+        currentLineWidth = 0;
     }
 
     public Size size() {
-        return size;
+        return new Size(width, length);
     }
 
 
--- src/test/java/day6/RoomTest.java
+++ src/test/java/day6/RoomTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.io.Reader;
+import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,9 +21,12 @@ class RoomTest {
                 ...
                 """;
 
-        var roomSize = new Room(roomMap).size();
+        var roomSize = new Room(from(roomMap)).size();
 
         assertThat(roomSize).isEqualTo(new Room.Size(3, 2));
     }
 
+    Reader from(String input) {
+        return new StringReader(input);
+    }
 }
