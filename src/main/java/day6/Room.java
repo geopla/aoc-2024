97bac6b90015f1ecb0ca61289e1e6f3ce8da5a78
Georg Plafinger <georg.plafinger@datev.de>
2025-01-30 17:29:03 +0100

    initialze obstacles and guard(s) from input

--- src/main/java/day6/Room.java
+++ src/main/java/day6/Room.java
@@ -1,32 +1,66 @@
 package day6;
 
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.IntConsumer;
+
+import static java.util.Collections.unmodifiableList;
 
 class Room {
 
     private int length = 0;
     private int width = 0;
-    int currentLineWidth = 0;
+
+    private int currentX = 0;
 
     record Size(int width, int length) { }
 
+    record Position(int x, int y) { }
+
+    record Obstruction(char type, Position position) { }
+
+    record Guard(char facing, Position position) { }
+
     private Size size;
 
+    private List<Obstruction> obstructions = new ArrayList<>();
+
+    private List<Guard> guards = new ArrayList<>();
     public Room(Reader input) {
         initializeFrom(input);
+    }
+
+    Size size() {
+        return new Size(width, length);
+    }
+
+    List<Obstruction> obstructions() {
+        return unmodifiableList(obstructions);
+    }
+
+    List<Guard> guards() {
+        return guards;
     }
 
     private void initializeFrom(Reader input) {
         Map.codePoints(input).forEach(new IntConsumer() {
             @Override
             public void accept(int codePoint) {
-                switch ((char) codePoint) {
-                    case '\n' -> {
-                        increaseLength();
-                        increaseWidthOnDemand();
-                    }
-                    default -> ++currentLineWidth;
+                if (isBorder(codePoint)) {
+                    increaseLength();
+                    increaseWidthOnDemand();
+                }
+                else if (isObstruction(codePoint)) {
+                    obstructions.add(new Obstruction((char) codePoint, new Position(currentX, length)));
+                    ++currentX;
+                }
+                else if (isGuard(codePoint)) {
+                    guards.add(new Guard((char) codePoint, new Position(currentX, length)));
+                    ++currentX;
+                }
+                else {
+                    ++currentX;
                 }
             }
         });
@@ -37,13 +71,19 @@ class Room {
     }
 
     private void increaseWidthOnDemand() {
-        width = Math.max(width, currentLineWidth);
-        currentLineWidth = 0;
+        width = Math.max(width, currentX);
+        currentX = 0;
     }
 
-    public Size size() {
-        return new Size(width, length);
+    boolean isObstruction(int type) {
+        return type == '#';
     }
 
+    boolean isGuard(int type) {
+        return type == '^';
+    }
 
+    boolean isBorder(int type) {
+        return type == '\n';
+    }
 }
--- src/test/java/day6/RoomTest.java
+++ src/test/java/day6/RoomTest.java
@@ -3,11 +3,8 @@ package day6;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 import java.io.Reader;
 import java.io.StringReader;
-import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -24,6 +21,41 @@ class RoomTest {
         var roomSize = new Room(from(roomMap)).size();
 
         assertThat(roomSize).isEqualTo(new Room.Size(3, 2));
+    }
+
+    @Test
+    @DisplayName("Should have obstacles read from map input NOT including the guard)")
+    void shouldHaveObstacles() {
+        var roomMap = """
+                #....
+                .#..#
+                ....#
+                """;
+
+        var room = new Room(from(roomMap));
+
+        assertThat(room.obstructions()).containsExactlyInAnyOrder(
+                new Room.Obstruction('#', new Room.Position(0, 0)),
+                new Room.Obstruction('#', new Room.Position(1, 1)),
+                new Room.Obstruction('#', new Room.Position(4, 1)),
+                new Room.Obstruction('#', new Room.Position(4, 2))
+        );
+    }
+
+    @Test
+    @DisplayName("Should have guard(s) - only one for now")
+    void shouldHaveGuards() {
+        var roomMap = """
+                #....
+                .#..#
+                ^...#
+                """;
+
+        var room = new Room(from(roomMap));
+
+        assertThat(room.guards()).containsExactly(
+              new Room.Guard('^', new Room.Position(0, 2))
+        );
     }
 
     Reader from(String input) {
