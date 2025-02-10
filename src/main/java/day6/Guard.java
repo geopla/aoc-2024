6543c2a5cb577409e24eb80d987ea556eac2a3d2
Georg Plafinger <georg.plafinger@datev.de>
2025-02-10 18:01:59 +0100

    introduce guard walk in preparation of a little redesign

--- src/main/java/day6/Guard.java
+++ src/main/java/day6/Guard.java
@@ -1,29 +1,76 @@
 package day6;
 
+import day6.Lifecycle.Computed;
+
+import java.util.LinkedHashSet;
+import java.util.stream.Stream;
+
 class Guard {
+
+    static class Walk {
+        private final LinkedHashSet<Leg<Computed>> legs = new LinkedHashSet<>();
+
+        boolean add(Leg<Computed> leg) {
+            tryValidateConnectivity(leg);
+
+            return legs.add(leg);
+        }
+
+        Stream<Leg<Computed>> legs() {
+            return legs.stream();
+        }
+
+        private void tryValidateConnectivity(Leg<Computed> leg) {
+            if (isNotEmptyWalk() && isNotPartOfWalk(leg)) {
+                var lastLegEnd = legs.getLast().end();
+                var newLegStart = leg.start();
+
+                if (! newLegStart.equals(lastLegEnd)) {
+                    throw new IllegalArgumentException("end %s not connected to start %s".formatted(lastLegEnd, newLegStart));
+                }
+            }
+        }
+
+        private boolean isNotEmptyWalk() {
+            return !legs.isEmpty();
+        }
+        private boolean isNotPartOfWalk(Leg<Computed> leg) {
+            return !legs.contains(leg);
+        }
+    }
 
     private final Room room;
     private final Room.Position startPosition;
     private final CardinalDirection startFacing;
 
-    private TurnStrategy turnStrategy;
+    private final TurnStrategy turnStrategy;
+
 
     Guard(Room room, Room.Position position, char facing) {
         this.room = room;
+        this.startPosition = position;
+        this.startFacing = CardinalDirection.from(facing);
+
+        // may be configurable in future
+        turnStrategy = new TurnRight();
+
         if (room == null) {
             throw new IllegalArgumentException("guard must be assigned to a room");
         }
-
-        startPosition = position;
         if (startPosition == null) {
             throw new IllegalArgumentException("guard must be placed on a start position");
         }
-
-        startFacing = CardinalDirection.from(facing);
-        turnStrategy = new TurnRight();
     }
 
-    public Room room() {
+    Walk walk() {
+        // TODO compute walk dependent on the room layout including historian added obstructions
+        // Step 1, copy implementation from Patrol
+        // Step 2, integrate looping paths
+        // Step 3, extend room to accept historian introduced obstacles too
+        return null;
+    }
+
+    Room room() {
         return room;
     }
 
--- src/test/java/day6/GuardTest.java
+++ src/test/java/day6/GuardTest.java
@@ -4,11 +4,76 @@ import org.instancio.Instancio;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static day6.CardinalDirection.NORTH;
+import static day6.CardinalDirection.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class GuardTest {
+
+    Lifecycle.Computed hitsObstruction = new Lifecycle.Computed(Terminator.OBSTRUCTION);
+
+    @Test
+    @DisplayName("Should accept a leg starting a guard walk")
+    void shouldAcceptAnInitialLeg() {
+        var walk = new Guard.Walk();
+        var leg = new Leg<>(new Room.Position(0, 0), SOUTH, 2, hitsObstruction);
+
+        assertThat(walk.add(leg)).isTrue();
+        assertThat(walk.legs()).containsExactly(leg);
+    }
+
+    @Test
+    @DisplayName("Should accept another leg connected to a guard walk")
+    void shouldAcceptAnotherConnectedLeg() {
+        var walk = new Guard.Walk();
+
+        var startLeg = new Leg<>(new Room.Position(0, 0), SOUTH, 2, hitsObstruction);
+        walk.add(startLeg);
+
+        var connectedLeg = new Leg<>(new Room.Position(0, 2), EAST, 3, hitsObstruction);
+        walk.add(connectedLeg);
+
+        assertThat(walk.legs()).containsExactly(
+                startLeg,
+                connectedLeg
+        );
+    }
+
+    @Test
+    @DisplayName("Should reject another NOT connected leg (for safety reasons)")
+    void shouldRejectAnotherNotConnectedLeg() {
+        var walk = new Guard.Walk();
+
+        var startLeg = new Leg<>(new Room.Position(0, 0), SOUTH, 2, hitsObstruction);
+        walk.add(startLeg);
+
+        var unconnectedLeg = new Leg<>(new Room.Position(1, 3), EAST, 2, hitsObstruction);
+
+        assertThatIllegalArgumentException().isThrownBy(() ->
+                        walk.add(unconnectedLeg)
+                )
+                .withMessage("end Position[x=0, y=2] not connected to start Position[x=1, y=3]");
+    }
+
+    @Test
+    @DisplayName("Should ignore a looping leg (without throwing an exception)")
+    void shouldIngnoreLoopingLeg() {
+        var walk = new Guard.Walk();
+
+        var twoStepsSouth = new Leg<>(new Room.Position(0, 0), SOUTH, 2, hitsObstruction);
+        walk.add(twoStepsSouth);
+
+        var twoStepsEast = new Leg<>(new Room.Position(0, 2), EAST, 2, hitsObstruction);
+        walk.add(twoStepsEast);
+
+        var twoStepsNorth = new Leg<>(new Room.Position(2, 2), NORTH, 2, hitsObstruction);
+        walk.add(twoStepsNorth);
+
+        var twoStepsWest = new Leg<>(new Room.Position(2, 0), WEST, 3, hitsObstruction);
+        walk.add(twoStepsWest);
+
+        assertThat(walk.add(twoStepsSouth)).isFalse();
+    }
 
     @Test
     @DisplayName("Should always be assigned to a room")
